af6da371993589b620e683dff00fffac577f7d55
rbasry29-blip <rbasry29@gmail.com>
2025-11-30 08:27:38 +0600

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -3,7 +3,7 @@ g='\e[92m'
 b='\e[34m'
 r='\e[0m'
 y='\e[1;33m'
-
+c='\e[1;96m'
 a(){
 
 clear
@@ -11,7 +11,7 @@ printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${g} Updating and Installing packages (xubuntu-desktop and dbus-x11) This may take up to 2 hours,  please wait${r}\n"
+printf "${b}[${g}*${b}]${c} Updating and Installing packages (xubuntu-desktop and dbus-x11) This may take up to 2 hours,  please wait${r}\n"
 
 # Login to Ubuntu and run commands sequentially
 proot-distro login ubuntu -- bash -c "dpkg --configure -a && apt update -y && apt install xubuntu-desktop dbus-x11 -y && printf '${b}[${g}*${b}]${g} Adding user ubuntu ${r}\n' && adduser --disabled-password --gecos '' ubuntu"
@@ -33,10 +33,10 @@ printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
 printf "${b}[${g}*${b}]${g} Finishing...
 Everything is good!
 You can launch Ubuntu via termux-x11 using commands:
-${y}1. server
+1. ${y}server${g}
 Then open another session and run -
-2. ubuntu
-3. gui
+2. ${y}ubuntu${g}
+3. ${y}gui${g}
 Enjoy! ${r}\n"
 
 }
