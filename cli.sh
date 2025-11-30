662b809d0511d7b8ea20c2c4f278029370c5c2b3
rbasry29-blip <rbasry29@gmail.com>
2025-11-30 08:24:25 +0600

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -2,13 +2,15 @@
 g='\e[92m'
 b='\e[34m'
 r='\e[0m'
+y='\e[1;33m'
+c='\e[1;96m'
 software(){
 clear
 printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${g} Updating and installing packages ${r}\n"
+printf "${b}[${g}*${b}]${c} Updating and installing packages ${r}\n"
 termux-setup-storage
 apt update -y && apt install proot-distro -y && apt install x11-repo  -y && apt install termux-x11-nightly -y
 apt install proot-distro -y
@@ -17,10 +19,10 @@ printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${g} Installing distro ${r}\n"
+printf "${b}[${g}*${b}]${c} Installing distro ${r}\n"
 proot-distro install ubuntu 
 
-printf "${b}[${g}*${b}]${g} Ubuntu installed! Now run another command ./gui.sh ${r}\n"
+printf "${b}[${g}*${b}]${g} Ubuntu installed! Now run another command ${y}./gui.sh ${r}\n"
 cat > $PREFIX/bin/server << 'EOF'
 #!/bin/bash
 echo "Starting server... started!"
