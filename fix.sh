876b4fe83e1c36d1bec59627b5b6b81a5cdb14ba
rbasry29-blip <rbasry29@gmail.com>
2025-12-02 20:08:33 +0600

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,1 +1,12 @@
+#!/bin/bash 
  
+
+
+
+
+clear
+printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
+printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
+printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
+printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
+printf "${b}[${g}*${b}]${c} Updating and Installing packages.  This may take up to 2 hours,  please wait${r}\n"
