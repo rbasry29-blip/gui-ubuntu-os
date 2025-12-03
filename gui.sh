d93a504dc493f4744e42806c5212de6c58dc193c
rbasry29-blip <rbasry29@gmail.com>
2025-12-03 18:35:28 +0600

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -28,6 +28,10 @@ printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
 printf "${b}[${g}*${b}]${g} Adding user ubuntu ${r}\n" 
 proot-distro login ubuntu -- adduser ubuntu
 
+printf "${b}[${g}*${b}]${g} Creating launchers  ${r}\n"
+
+proot-distro login ubuntu -- apt update -y
+
 
 # Create gui launcher inside Ubuntu
 proot-distro login ubuntu -- bash -c "cat > /usr/bin/gui << 'EOF'
@@ -43,12 +47,12 @@ printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${g} Finishing...
+printf "${b}[${g}*${b}]${c} Finishing...
 Everything is good!
 You can launch Ubuntu via termux-x11 using commands:
-1. ${y}server${g}
+1. ${y}server${c}
 Then open another session and run -
-2. ${y}ubuntu${g}
-3. ${y}gui${g}
+2. ${y}ubuntu${c}
+3. ${y}gui${c}
 Enjoy! ${r}\n"
 
