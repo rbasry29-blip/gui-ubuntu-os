cc30b2d923e5e7aeba1c92bea4b3dadc542b3850
linuxdevoloper <rbasry29@gmail.com>
2025-12-29 10:40:09 +0600

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -4,31 +4,30 @@ b='\e[34m'
 r='\e[0m'
 y='\e[1;33m'
 c='\e[1;96m'
-commands(){                                                
-type_effect () {
-    local text="$1"
-    local delay=$2                                         
-    for ((i=0; i<${#text}; i++)); do                               printf "${c}${text:$i:1}${r}"
-        sleep "$delay"                                         done
-    echo                                                   }
-clear                                                      printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
+
+clear
+printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
 printf "$c"
-type_effect "Welcome" 0.04
-sleep 0.2
-type_effect "Hello , User" 0.08
-sleep 0.5
-type_effect "May your work will easier" 0.08
-sleep 0.7
-type_effect "Let's get started!" 0.08
-sleep 0.2
-type_effect "Enjoy proot-distro-ubuntu" 0.04
+LINES=(
+  "Welcome"
+  "May your work will easier"
+  "Let's get started!"
+  "Enjoy proot-distro-ubuntu"
+)
 
+for t in "${LINES[@]}"; do
+  for ((i=0;i<${#t};i++)); do
+    printf "${C}${B}${t:i:1}${N}"
+    ((RANDOM%3==0)) && { printf "${G} ${N}\b"; sleep 0.05; }
+    sleep 0.08
+  done
+  echo
+  sleep 0.4
+done
 printf "$r"
-sleep 2
-
 dpkg --configure -a
 apt --fix-broken install
 clear
@@ -77,6 +76,6 @@ proot-distro login ubuntu --shared-tmp -- su - ubuntu
 EOF
 
 chmod +x $PREFIX/bin/ubuntu
-
 }
+
 commands
